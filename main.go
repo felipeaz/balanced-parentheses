06c6b3ad1c353c3370736e8cc81381029b14f75d
Felipe Azevedo <felipe9_azevedo@hotmail.com>
2022-06-27 18:52:56 -0300

    implemented first version using first in last out

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+)
+
+var (
+	openSignalMapper = map[string]string{
+		"(": ")",
+		"{": "}",
+		"[": "]",
+	}
+)
+
+func main() {
+	inputs := []string{
+		// balanced
+		"()",
+		// balanced
+		"([]){}",
+		// not balanced
+		"([",
+		// not balanced
+		"([]{}]",
+	}
+	for _, i := range inputs {
+		b := isBalanced(i)
+		log.Printf("%s balanced? %v\n", i, b)
+	}
+}
+
+func isBalanced(str string) bool {
+	var toClose []string
+	for _, ch := range str {
+		chStr := string(ch)
+		_, ok := openSignalMapper[chStr]
+		if !ok {
+			if len(toClose) == 0 {
+				return false
+			}
+			// F.I.L.A - First In, Last Out
+			numOfOpenedSignals := len(toClose) - 1
+			expectedSignal, ok := openSignalMapper[toClose[numOfOpenedSignals]]
+			if !ok {
+				return false
+			}
+			if expectedSignal != chStr {
+				return false
+			}
+			// remove the last element
+			toClose = toClose[:numOfOpenedSignals]
+		} else {
+			toClose = append(toClose, chStr)
+		}
+	}
+	if len(toClose) > 0 {
+		return false
+	}
+	return true
+}
